20a72d8193afd3cb1a99a1b14a743ae5aecc5a81
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2023-10-21 17:55:32 -0300

    feat(app): add mock data

---
+++ src/app/data_mock/dataMock.ts
@@ -0,0 +1,26 @@
+export const DATA = [
+  {
+    "id": "1",
+    "image": "https://th.bing.com/th/id/R.400c4c818ecc8f76508497d8258b59e6?rik=InqVaBkEcOZkew&pid=ImgRaw&r=0",
+    "title": "Titulo bonito",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti illodeleniti quae ullam nostrum eum vitae praesentium obcaecati exercitationem molestiae vel laborum alias, ad et sapiente eveniet quos!Quae, assumenda?"
+  },
+  {
+    "id": "2",
+    "image": "https://th.bing.com/th/id/R.400c4c818ecc8f76508497d8258b59e6?rik=InqVaBkEcOZkew&pid=ImgRaw&r=0",
+    "title": "Titulo bonito",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti illodeleniti quae ullam nostrum eum vitae praesentium obcaecati exercitationem molestiae vel laborum alias, ad et sapiente eveniet quos!Quae, assumenda?"
+  },
+  {
+    "id": "3",
+    "image": "https://th.bing.com/th/id/R.400c4c818ecc8f76508497d8258b59e6?rik=InqVaBkEcOZkew&pid=ImgRaw&r=0",
+    "title": "Titulo bonito",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti illodeleniti quae ullam nostrum eum vitae praesentium obcaecati exercitationem molestiae vel laborum alias, ad et sapiente eveniet quos!Quae, assumenda?"
+  },
+  {
+    "id": "4",
+    "image": "https://th.bing.com/th/id/R.400c4c818ecc8f76508497d8258b59e6?rik=InqVaBkEcOZkew&pid=ImgRaw&r=0",
+    "title": "Titulo bonito",
+    "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti illodeleniti quae ullam nostrum eum vitae praesentium obcaecati exercitationem molestiae vel laborum alias, ad et sapiente eveniet quos!Quae, assumenda?"
+  },
+]

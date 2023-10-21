fe67e7c449f2486229d523146a432f4046143d57
Caio Ferraz <caioferrazalmeida.27@gmail.com>
2023-10-21 17:53:56 -0300

    feat:  Add dynamic Props

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class BigCardComponent {
 
+  @Input() id:string = '0';
+  @Input() image:string = '';
+  @Input() title:string = '';
+  @Input() description:string = '';
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class SmallCardComponent {
 
+  @Input() id :string = '';
+  @Input() image :string = '';
+  @Input() title:string = '';
 }
